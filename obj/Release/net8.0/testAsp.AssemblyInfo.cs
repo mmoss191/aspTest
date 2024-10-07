a100219fee5f58d50fceebdc16532785ccdc1930
Maximilian Moss <maximilian.moss@outlook.de>
2024-10-07 22:22:45 +0200

    test

--- obj/Release/net8.0/testAsp.AssemblyInfo.cs
+++ obj/Release/net8.0/testAsp.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("testAsp")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcc78b5829f04eb2bd40ff0b9622f235588cc9d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("testAsp")]
