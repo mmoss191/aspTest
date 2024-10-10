44c3a3e6f8814bb1deafddd2182feea1ea6da1a0
Maximilian Moss <maximilian.moss@outlook.de>
2024-10-10 23:08:47 +0200

    changedHeader

--- obj/Debug/net8.0/testAsp.AssemblyInfo.cs
+++ obj/Debug/net8.0/testAsp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("testAsp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcc78b5829f04eb2bd40ff0b9622f235588cc9d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76f96e43687469503f5f87d287021dacf3ad496b")]
 [assembly: System.Reflection.AssemblyProductAttribute("testAsp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("testAsp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
